8955bccfcb825d6282711d7d6fbf848a1b9413a3
toansbtc <toansbtc201@gmail.com>
2026-07-04 15:44:53 +0700

    edit cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ["http://localhost:3000", "https://bao-bi-binh-duong.vercel.app"] });
+  app.enableCors({
+    origin: ["http://localhost:3000", "https://bao-bi-binh-duong.vercel.app"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
